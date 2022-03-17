af7ae29034bc3b48ce709a2ba912b49fb811ea12
Maxim <balan.maxim@gmail.com>
2022-03-17 19:44:18 +0000

    comment

--- src/main/java/strings/OneAwayEditV1.java
+++ src/main/java/strings/OneAwayEditV1.java
@@ -7,7 +7,7 @@ package strings;
  **/
 public class OneAwayEditV1 {
     public static void main(String[] args) {
-        /**
+        /*
          * There are three types of edits that can be performed on strings: insert a character,
          * remove a character, or replace a character. Given two strings, write a function to check if they are
          * one edit (or zero edits) away.
